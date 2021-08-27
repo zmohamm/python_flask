4251ac630f752eccbfbe83c90b4bfa2eac369af9
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-27 13:28:14 +0530

    Update app.py

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template , url_for
+app = Flask(__name__)
 
+
+#@app.route("/")
+@app.route("/layout")
+def layout():
+     return render_template('layout.html')
+
+@app.route("/")
+@app.route("/home")
+def home():
+   return render_template('home.html')
+
+@app.route("/Resources")
+def Resources():
+   return render_template('Resources.html')
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port='5006',debug=True)
